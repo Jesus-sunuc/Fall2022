12c49ac897c9446431e04df9b82d16ee200d2b2b
Jesus-sunuc <74077822+Jesus-sunuc@users.noreply.github.com>
2022-12-26 22:03:34 -0600

    Add files via upload

---
+++ project-ch-2-Jesus-sunuc-master/Project2.Main/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Name: Jesus Sunuc
+Date: 09/15/2022
+// Description: The user will enter some numbers and the program will display a polynial, if the user enter a value for x, the program will display the result of the polynomial.
+*/
+namespace classes;
+using System.Diagnostics;
+using System;
+class Program
+{
+    // This is the main method.
+    static void Main(string[] args)
+    {
+        Greetings(); // Call the Greetings method.
+
+        string input = Console.ReadLine(); // Read the input from the console.
+        int n = int.Parse(input);
+        double[] a = new double[++n]; // Create an array of doubles with n+1 elements
+
+        for (int i = 0; i < n; i++) // Read the coefficients from the console
+        {
+            Console.Write("Coefficient for x^" + i + " = "); // Write the message to the console
+            a[i] = double.Parse(Console.ReadLine()); // Read the input from the console
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Here is your polynomial: " + Polynomial.toString(a)); // Write the message to the console and call the toString method from the Polynomial class.
+        Console.WriteLine();
+
+        Console.Write("Enter the value for x : "); // Write the message to the console
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("The result is : ");
+        Console.Write(Polynomial.horner(a, x)); // Write the result of the horner method from the Polynomial class.
+        Console.WriteLine();
+        Tests(); // Call the Tests method.
+    }
+
+    static void Tests() // This is the test method.
+    {
+        double[] a = { 1, 2, 3, 4, 5 };
+        double x = 2;
+        Debug.Assert(Polynomial.horner(a, x) == 57);
+        Debug.Assert(Polynomial.toString(a) == "1x^0 + 2x^1 + 3x^2 + 4x^3 + 5x^4 + ");
+        Console.WriteLine("Tests passed");
+    }
+
+    // This is the Greetings method
+    static void Greetings()
+    {
+        Console.WriteLine("Please enter the polynomial you wish to work with.");
+        Console.WriteLine("You will be prompted to enter the coeffient for each term in the polynomial.");
+        Console.WriteLine("You may enter zero if the term is absent from the polynomial.");
+        Console.WriteLine("Enter the degree of your polynomial: ");
+    }
+}
---
+++ project-ch-2-Jesus-sunuc-master/classes/Polynomial.cs
@@ -0,0 +1,37 @@
+﻿namespace classes;
+public class Polynomial // This is the Polynomial class
+{
+    private double[] Coefficients; // This is the array of coefficients
+    private Polynomial(double[] coefficients) // This is the costructor
+    {
+        Coefficients = coefficients;
+    }
+    public static double horner(double[] coefficients, double x) // This is the horner method that returns a double value.
+    {
+        double result = 0; // This is the result variable.
+
+        result = coefficients[0] * x; // This is the first step of the horner method.
+
+        for (int i = 1; i < (coefficients.Length - 1); i++) // This is the loop that calculates the result.
+        {
+            result += coefficients[i]; 
+            result *= x;
+        }
+
+        result += coefficients[coefficients.Length - 1];
+        return result; // This is the return statement.
+    }
+
+    public static string toString(double[] coefficients) // This is the toString method that returns a string value.
+    {
+        string result = "";
+        for (int i = 0; i < coefficients.Length; i++) // This is the loop that calculates the result.
+        {
+            if (coefficients[i] != 0)
+            {
+                result += coefficients[i] + "x^" + i + " + ";
+            }
+        }
+        return result;
+    }
+}
