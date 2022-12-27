4730735ac48a554a451e3a5cd7e53647be6d2d27
Jesus-sunuc <74077822+Jesus-sunuc@users.noreply.github.com>
2022-12-26 22:04:07 -0600

    Add files via upload

---
+++ project-ch-4-Jesus-sunuc-master/Project5.Main/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+Name: Jesus Sunuc
+Date: 09/26/2022
+Description: This program will create a date object and increment it by one day.
+*/
+
+namespace Date;
+class Program
+{
+    public static void Main() // Main method.
+    {
+        Date.Greetings(); // Call the greetings method.
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1. It will show you the date in this format: MM/DD/YYYY (e.g. April/01/2021)");
+        Console.WriteLine("2. It will show you the date in this format: MM/DD/YYYY (e.g. 04/01/2021)");
+        Console.WriteLine("3. Enter the month, day, and year you want to replace it with, and it will show you the the date incremented by one day.");
+        Console.WriteLine("4. Exit");
+        Console.WriteLine();
+
+        string x = Console.ReadLine();
+        int intputValue = int.Parse(x);
+
+        switch (intputValue) // Switch statement.
+        {
+            case 1: // Case 1.
+                Console.WriteLine();
+                Console.WriteLine("Enter the month represented by a number (e.g. 1 for January, 2 for February, etc.):");
+                string val1 = Console.ReadLine();
+                int value1 = int.Parse(val1);
+                Console.WriteLine("Enter the day (e.g. 1, 2, 3, etc.):");
+                string val2 = Console.ReadLine();
+                int value2 = int.Parse(val2);
+                Console.WriteLine("Enter the year (e.g. 2021, 2022, etc.):");
+                string val3 = Console.ReadLine();
+                int value3 = int.Parse(val3);
+                Date date1 = new Date(value1, value2, value3); // Create a date object.
+                Console.WriteLine("The date is: " + date1.GetMonth() + " " + date1.GetDay() + ", " + date1.GetYear()); // Display the date.
+                break;
+
+            case 2: // Case 2.
+                Console.WriteLine();
+                Console.WriteLine("Enter the month represented by a number (e.g. 1, 2, 3, etc):");
+                string val4 = Console.ReadLine();
+                int value4 = int.Parse(val4);
+                Console.WriteLine("Enter the day (e.g. 1, 2, 3, etc.):");
+                string val5 = Console.ReadLine();
+                int value5 = int.Parse(val5);
+                Console.WriteLine("Enter the year (e.g. 2021, 2022, etc.):");
+                string val6 = Console.ReadLine();
+                int value6 = int.Parse(val6);
+                Date date2 = new Date(value4, value5, value6); // Create a date object.
+                Console.WriteLine("The date is: " + date2.GetMonth2() + "/" + date2.GetDay() + "/" + date2.GetYear()); // Display the date.
+                break;
+
+            case 3: // Case 3.
+                Console.WriteLine();
+                Console.WriteLine("Enter the month represented by its name (e.g. 1 for January, 2 for February, etc.):");
+                string val7 = Console.ReadLine();
+                int value7 = int.Parse(val7);
+                Console.WriteLine("Enter the day (e.g. 1, 2, 3, etc.):");
+                string val8 = Console.ReadLine();
+                int value8 = int.Parse(val8);
+                Console.WriteLine("Enter the year (e.g. 2021, 2022, etc.):");
+                string val9 = Console.ReadLine();
+                int value9 = int.Parse(val9);
+                Date date3 = new Date(value7, value8, value9); // Create a date object.
+
+                Console.Write("The change date is: ");
+                date3.ChangeMonth(value7); // Change the month.
+                date3.ChangeDay(value8); // Change the day.
+                date3.ChangeYear(value9); // Change the year.
+
+                for (int i = 0; i <= 31; i++) // Increment the date by one day.
+                {
+                    date3.IncrementDateByOne();
+                    Console.WriteLine(date3.GetMonth() + "/" + date3.GetDay() + "/" + date3.GetYear());
+                }
+                break;
+
+            case 4: // Case 4.
+                Console.Clear();
+                break;
+        }
+    }
+}
---
+++ project-ch-4-Jesus-sunuc-master/Project5/Date.cs
@@ -0,0 +1,168 @@
+﻿/*
+Name: Jesus Sunuc
+Date: 09/26/2022
+Description: This program will create a date object and increment it by one day.
+*/
+
+namespace Date;
+public class Date // Date class
+{
+    private int Day;
+    private int Month;
+    private int Year;
+
+    public Date(int month, int day, int year) // Date constructor
+    {
+        Month = month;
+        Day = day;
+        Year = year;
+    }
+    public int GetDay() // GetDay method returns the day.
+    {
+        return Day;
+    }
+
+    public string GetMonth() // GetMonth method returns the month.
+    {
+        switch (Month)
+        {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                return "Invalid Month";
+        }
+    }
+
+    public int GetMonth2() // GetMonth method returns the month.
+    {
+        return Month;
+    }
+
+    public int GetYear() // GetYear method returns the year.
+    {
+        return Year;
+    }
+
+    public void ChangeDay(int day) // ChangeDay method changes the day.
+    {
+        Day = day;
+    }
+
+    public int ChangeMonth(int changeMonth) // ChangeMonth method changes the month.
+    {
+        return changeMonth;
+    }
+
+    public void ChangeYear(int year) // ChangeYear method changes the year.
+    {
+        Year = year;
+    }
+
+    public bool MonthOf30Days() // MonthOf30Days method returns true if the month has 30 days.
+    {
+        if (Month == 3 || Month == 4 || Month == 6 || Month == 9 || Month == 11)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool MonthOf31Days() // MonthOf31Days method returns true if the month has 31 days.
+    {
+        if (Month == 2)
+        {
+            return false;
+        }
+
+        if (MonthOf30Days())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void IncrementDateByOne() // IncrementDateByOne method increments the date by one day.
+    {
+        if (Day <= 27)
+        {
+            Day++;
+        }
+
+        else if (Day == 28 && Month != 2)
+        {
+            Day++;
+        }
+
+        else if (Day == 28 && Month == 2)
+        {
+            Day = 1;
+            Month = 3;
+        }
+
+        else if (Day == 29)
+        {
+            Day++;
+        }
+
+        else if (Day == 30 && MonthOf31Days())
+        {
+            Day++;
+        }
+
+        else if (Day == 30 && MonthOf30Days())
+        {
+            Day = 1;
+            Month++;
+        }
+
+        else if (Day == 31 && MonthOf31Days())
+        {
+            Day = 1;
+            Month++;
+        }
+
+        else if (Day == 31 && Month != 12)
+        {
+            Day = 1;
+            Month++;
+        }
+
+        else if (Day == 31 && Month == 12)
+        {
+            Day = Month = 1;
+            Year++;
+        }
+    }
+
+    public static void Greetings() // Greetings method.
+    {
+        Console.WriteLine("Welcome to the date calendar!");
+        Console.WriteLine("This program will create a date object and increment it by one day.");
+    }
+}
